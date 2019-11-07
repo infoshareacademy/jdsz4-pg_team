4537e1c671f9951971ce1569d2327338eb1b3b07
Jakub Szczyglewski <j.szczyglewski@gmail.com>
2019-11-07 19:13:05 +0100

    corrections

--- sql_project/environment/variance_analysis.sql
+++ sql_project/environment/variance_analysis.sql
@@ -1,5 +1,3 @@
----------------------------------------------------------------------
-
 select CountryName, IndicatorName, variance(value) as wariancja, stddev(value)/avg(Value), stddev(value), count(1)
 from environment_selection
 where value != 0
@@ -7,27 +5,28 @@ where value != 0
     and topic not like 'Environment: Land%'
     and Topic not like 'Environment: Density%'
     and Topic not ilike 'Environment: Agricul%'
-group by CountryName, IndicatorName
+group by IndicatorName, CountryName
 having stddev(value) is not null and count(1)>10
 order by 3 desc;
 
 --Electricity production from renewable sources, excluding hydroelectric (kWh)
--- jest to wskaznik dla ktorego wariancja jest najwyzsza, czyli wystapila najwieksza ilosciowa zmiana
+-- variance for this indicator is the highest
 
-select CountryName, IndicatorName, variance(value) as wariancja, stddev(value)/avg(Value), stddev(value), count(1)
+select CountryName, IndicatorName, variance(value) as wariancja, stddev(value)/avg(Value), topic, stddev(value), count(1)
 from environment_selection
 where value != 0
     and topic not like 'Environment: Fres%'
     and topic not like 'Environment: Land%'
     and Topic not like 'Environment: Density%'
     and Topic not ilike 'Environment: Agricul%'
-group by CountryName, IndicatorName
+    and Topic not ilike 'Environment: Biodiv%'
+group by topic, CountryName, IndicatorName
 having stddev(value) is not null and count(1)>10
 order by 4 desc;
 
--- Po sprawdzeniu kolejnosci wedlug stosunku odchylenia do sredniej, wciaz jest to jeden z czynnikow o najwyzszym wskazniku.
+-- after checking the stddev(value)/avg(Value) this is still one of highest indicators - definitely worth looking at
 
--- kwartyle dla wskaznika 'Electricity production from renewable sources, excluding hydroelectric (kWh)'
+-- quartills for indicators  'Electricity production from renewable sources, excluding hydroelectric (kWh)'
 select CountryName, IndicatorName,
        percentile_disc(array[0.25, 0.5, 0.75])
            within group (order by Value) as kwartyle
@@ -35,6 +34,5 @@ from environment_selection
 where CountryName = 'China' -- country
   and IndicatorName like 'Electricity production from renewable sources, excluding hydroelectric (kWh)' --indicator
 group by 1, 2
-
---jest to rozklad lewostronny
 ----------------------
+
