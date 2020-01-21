8be50b39384267db99a0665a35f2821f36816629
Jakub Szczyglewski <j.szczyglewski@gmail.com>
2020-01-21 23:12:27 +0100

    added new lines to make printouts more readable

--- python_project/Battle Simulator/mechanika-gry/mechanika.py
+++ python_project/Battle Simulator/mechanika-gry/mechanika.py
@@ -2,7 +2,7 @@ def create_pokemon():
     import pandas as pd
     df_pokemons = pd.read_csv('main_Pokemon.csv')
     while True:
-        print('Enter name of Pokemon.')
+        print('\nEnter name of Pokemon.')
         name = str(input()).capitalize()
         if name in list(df_pokemons['pokename']):
             while True:
@@ -15,7 +15,7 @@ def create_pokemon():
                     att = int(df_pokemons[(df_pokemons['pokename'] == name) & (df_pokemons['pokelevel'] == level)]['attack'].mean())
                     defense = int(df_pokemons[(df_pokemons['pokename'] == name) & (df_pokemons['pokelevel'] == level)]['defense'].mean())
                     type1 = str(df_pokemons[(df_pokemons['pokename'] == name) & (df_pokemons['pokelevel'] == level)]['type1'].unique()[0])
-                    print('{} on level {}: \nType = {} \nHP = {} \nDefense = {} \nSpeed = {}'.format(name, level, type1, hp, defense, speed))
+                    print('{} on level {}: \nType = {} \nHP = {} \nDefense = {} \nSpeed = {}\nAttack = {}\n'.format(name, level, type1, hp, defense, speed,att))
                     return type1, name, level, speed, hp, defense, att
                 except ValueError:
                     print("Oops! I don't think we got {} on level {} is in the archive.\nWould you like to choose other Pokemon?".format(name, level))
